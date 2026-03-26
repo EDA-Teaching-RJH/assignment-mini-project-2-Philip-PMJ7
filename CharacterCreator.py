ed214fec242cbf5a4f2ebbd56aeda216d1f126db
Philip-PMJ7 <pmj7@kent.ac.uk>
2026-03-26 23:11:25 +0000

    Save 3
     - Added the display function to the Event subclass. Still figuring out linked_characters.
     - Added the entire Character subclass with all the planned functions so far.

--- CharacterCreator.py
+++ CharacterCreator.py
@@ -28,10 +28,52 @@ class Event(StoryElement):
     def __init__ is for setting up the Subclass.
     def add_character will add the ID of a character to the event, and will display which characters are linked to the event.
     def remove_character will remove the ID of a character from the event.
+    def update_time will ask for a new start/end date
     def display will display all the information of the event, from the name to the dates to the time.'''
     def __init__(self, ID, name, description, start_date, end_date, linked_characters):
         super().__init__(ID, name, description)
         self.start_date = start_date
         self.end_date = end_date
         self.date = start_date
-        self.linked_characters =
+        self.linked_characters = linked_characters
+    
+    def add_character():
+        sdsad
+
+    def remove_character():
+        sdasdasd
+
+    def display(self):
+        super().display_basic_info
+        print(f"Start Date: {self.start_date}")
+        print(f"End Date: {self.end_date}")
+        print(f"Linked Characters: {self.linked_characters}")
+
+class Character(StoryElement):
+    '''Character is a Subclass of StoryElement.
+    Its purpose is to add a character to this writing tool. It lets you add more information for a character, like their role in the story, their species (non_humans?) and skills/powers.
+    def __init__ is for setting up the Subclass.
+    def update_species will update what species the character is.
+    def update_role will update what role they are.
+    def update_abilities updates their abilities.
+    def display lists all of a character's information.'''
+    def __init__(self, ID, name, description, species, role, abilities):
+        super().__init__(ID, name, description)
+        self.species = species
+        self.role = role
+        self.abilities = abilities
+
+    def update_species(self, new_species):
+        self.species = new_species
+    
+    def update_role(self, new_role):
+        self.role = new_role
+    
+    def update_abilities(self, new_abilities): #This may be replaced by a tuple, which can give each ability an individual description.
+        self.abilities = new_abilities #That will also let each ability be modular, adding and subtracting each one individually.
+    
+    def display(self):
+        super().display_basic_info
+        print(f"Species: {self.species}")
+        print(f"Role: {self.role}")
+        print(f"Abilities: {self.abilities}")
